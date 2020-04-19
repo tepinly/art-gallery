4505a03a4f4195ec83c4f8925e7043d88612d7b1
Youssef Fadel <youssef_a_fadel@hotmail.com>
2020-04-19 02:11:33 +0200

    Update

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 AOS.init({
-    duration: 800,
+    duration: 1000,
   })
+
+// This function will show the image in the lightbox
+var zoomImg = function () {
+  // Create evil image clone
+  var clone = this.cloneNode();
+  clone.classList.remove("zoomD");
+
+  // Put evil clone into lightbox
+  var lb = document.getElementById("lb-img");
+  lb.innerHTML = "";
+  lb.appendChild(clone);
+
+  // Show lightbox
+  lb = document.getElementById("lb-back");
+  lb.classList.add("show");
+};
+
+window.addEventListener("load", function(){
+  // Attach on click events to all .zoomD images
+  var images = document.getElementsByClassName("Img");
+  if (images.length>0) {
+    for (var img of images) {
+      img.addEventListener("click", zoomImg);
+    }
+  }
+
+  // Click event to hide the lightbox
+  document.getElementById("lb-back").addEventListener("click", function(){
+    this.classList.remove("show");
+  })
+});
